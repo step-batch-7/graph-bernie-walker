45c61a9226127a2b2fb8bafeeac743d32a65eb52
Bernard Nehal D'Cunha <58025656+bernie-walker@users.noreply.github.com>
2020-06-11 14:39:38 +0530

    added detect path and modified bfs to find the path

--- src/graph.js
+++ src/graph.js
@@ -4,6 +4,45 @@
 // To => to
 // Should return true.
 
-const bfs = function (pairs, source, target) {};
+const detectPaths = function (paths, pair) {
+  const [from, to] = pair;
+
+  if (!paths[from]) {
+    paths[from] = [];
+  }
+
+  paths[from].push(to);
+
+  return paths;
+};
+
+const bfs = function (pairs, source, target) {
+  const paths = pairs.reduce(detectPaths, {});
+  const visited = [];
+  const toVisit = [source];
+
+  while (toVisit.length) {
+    const vertexToProcess = toVisit.shift();
+    const adjacentVertices = paths[vertexToProcess];
+
+    if (!adjacentVertices) {
+      continue;
+    }
+
+    if (adjacentVertices.includes(target)) {
+      return true;
+    }
+
+    adjacentVertices.forEach((vertex) => {
+      if (!visited.concat(toVisit).includes(vertex)) {
+        toVisit.push(vertex);
+      }
+    });
+
+    visited.push(vertexToProcess);
+  }
+
+  return false;
+};
 
 module.exports = { bfs };
--- test/testGraph.js
+++ test/testGraph.js
@@ -2,17 +2,39 @@ const { assert } = require('chai');
 const { bfs } = require('../src/graph');
 
 describe('bfs', function () {
-  it('should return false if the source vertex has no outgoing edges', function () {});
+  const testData = [
+    ['nn', 'oo'],
+    ['nn', 'nn'],
+    ['mm', 'oo'],
+    ['mm', 'll'],
+    ['ll', 'jj'],
+    ['jj', 'kk'],
+    ['kk', 'ii'],
+    ['kk', 'jj'],
+    ['kk', 'mm'],
+  ];
 
-  it('should return false when the target vertex has no incoming edges', function () {});
+  it('should return false if the source vertex has no outgoing edges', function () {
+    assert.isFalse(bfs(testData, 'oo', 'mm'));
+  });
 
-  it('should return true when two vertices are directly connected to each other', function () {});
+  it('should detect a edge between adjacent vertices', function () {
+    assert.isTrue(bfs(testData, 'mm', 'll'));
+  });
 
-  it('should detect the edge between two non-adjacent vertices', function () {});
+  it('should report absence of an edge between two nodes', function () {
+    assert.isFalse(bfs(testData, 'nn', 'mm'));
+  });
 
-  it('should be able to detect the edges when there is looping in the path', function () {});
+  it('should detect the edge between two non-adjacent vertices', function () {
+    assert.isTrue(bfs(testData, 'mm', 'jj'));
+  });
 
-  it('should detect an edge from a vertex to itself if does not exist', function () {});
+  it('should be able to detect the edges when there is looping in the path', function () {
+    assert.isTrue(bfs(testData, 'mm', 'ii'));
+  });
 
-  it('should detect an edge from a vertex to itself if exists', function () {});
+  // it('should detect an edge from a vertex to itself if exists', function () {});
+
+  // it('should report absence of an edge from a vertex to itself', function () {});
 });
